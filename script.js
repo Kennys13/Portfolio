b7fe891ca0c373b7ebabbe7e395be0e5c4c27061
Bhupati Parmar <87934381+Kennys13@users.noreply.github.com>
2024-10-02 12:08:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -80,3 +80,35 @@ window.addEventListener('DOMContentLoaded', (event) => {
     createAnimatedPieChart('dsaChart', 'DSA', 70, '#2ecc71');
     createAnimatedPieChart('mernChart', 'MERN', 90, '#34495e');
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        let target = document.querySelector(this.getAttribute('href'));
+        let headerOffset = 70; // Height of your fixed header
+        let elementPosition = target.offsetTop;
+        let offsetPosition = elementPosition - headerOffset;
+
+        window.scrollTo({
+            top: offsetPosition,
+            behavior: "smooth"
+        });
+    });
+});
+
+window.addEventListener('load', function() {
+    const loader = document.getElementById('loader');
+    setTimeout(() => {
+        loader.style.animation = 'fadeOut 1s ease';
+        loader.addEventListener('animationend', () => {
+            loader.style.display = 'none';
+        });
+    }, 500); // Loader will fade out after 6 seconds
+});
+
+document.getElementById('portfolio-link').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent the default anchor behavior
+    window.location.href = window.location.pathname + '#home'; // Change the hash to '#home'
+    window.location.reload(); // Refresh the page
+});
